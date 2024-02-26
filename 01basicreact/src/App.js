9059155d6ff4d2a4bf58d3a02737f088dcc5eb6b
harsh sharma <harsh313008@gmail.com>
2024-02-26 11:11:38 +0530

    basic jsx and js , making of components

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,11 @@
 
-
+import Chai from "./chai";
 function App() {
   return (
-    <h1>Hello world</h1>
+    
+    <Chai />
+    
+    
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,14 @@
 
 
-
+import Chai from "./chai"
 function App() {
  
 
   return (
-    <h1>hello world with Vite </h1>
+    <>
+    <h1>Hello world</h1>
+    <Chai/>{/* jsx can only return one element so to return many tag we wrap our whole in a div or fragment*/}
+    </>
   )
 }
 
